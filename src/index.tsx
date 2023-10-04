07b852927c92878f9d170b4059ea5dd254752d2e
azuma164 <ornitho1027@gmail.com>
2023-10-04 16:49:17 -0700

    add build

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='http://hirokiazuma164.me'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
